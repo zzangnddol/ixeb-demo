7cfbe69fe84522fbab623e0fe22f4280623dda32
송영배 <ybsong@msn.com>
2018-12-13 17:57:08 +0900

    ixeb server slice URL 정보 추가

--- src/main/webapp/common/common.js
+++ src/main/webapp/common/common.js
@@ -1,2 +1,2 @@
-// var IXEB_SERVICE_URL = 'http://10.1.91.156:8080/ixeb-manager/service.ixeb';
 var IXEB_SERVICE_URL = '/ixeb-demo/service.ixeb';
+var IXEB_SERVICE_SLICE_URL = '/ixeb-demo/service.slice.ixeb';
